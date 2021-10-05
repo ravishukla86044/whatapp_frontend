2cc7c5ca6eaacdebe13d6b873f7606bca23161af
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-05 21:44:29 +0530

    background added as postion absolute

--- src/Components/RightBox/Rightbox.jsx
+++ src/Components/RightBox/Rightbox.jsx
@@ -4,6 +4,8 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AttachFileOutlinedIcon from "@material-ui/icons/AttachFileOutlined";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { Message } from "./Message";
+import whatsappDark from "../../Assets/whatsappDark.png";
+import whatsappLight from "../../Assets/whatsappLight.png";
 
 function RightBox() {
   return (
@@ -27,6 +29,7 @@ function RightBox() {
         </div>
       </div>
       <div className="messagesDiv">
+        <div className="background"></div>
         <Message />
         <Message />
       </div>
@@ -60,6 +63,33 @@ const Right = styled.div`
   }
 
   .messagesDiv {
+    flex: 1;
+    position: relative;
+
+    padding: 30px;
+    overflow: auto;
+    background-color: rgb(229, 221, 213);
+    ::-webkit-scrollbar {
+      width: 6px;
+      height: 6px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+    ::-webkit-scrollbar-track {
+      background: hsla(0, 0%, 100%, 0.1);
+    }
+  }
+  .background {
+    position: absolute;
+    opacity: 0.1;
+    background-repeat: repeat;
+    background-position: center;
+    background: url(${whatsappLight});
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
   }
 `;
 export { RightBox };
