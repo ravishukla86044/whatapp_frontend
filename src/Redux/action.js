43418aa866f417014a0cb155dd69482eeeb9110f
ravishukla86044 <ravishukls86044@gmail.com>
2021-10-06 18:49:16 +0530

    redux added

---
+++ src/Redux/action.js
@@ -0,0 +1,97 @@
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  REGISTER_FAILURE,
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  UPDATE_PROFILE,
+} from "./actionType";
+import axios from "axios";
+
+//....................updateProfile......................//
+const updateProfile = (payload) => {
+  return {
+    type: UPDATE_PROFILE,
+    payload,
+  };
+};
+
+//..........................................Login.................................//
+const loginRequest = () => {
+  return {
+    type: LOGIN_REQUEST,
+  };
+};
+const loginSuccess = (payload) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload,
+  };
+};
+
+const loginFailure = (payload) => {
+  return {
+    type: LOGIN_FAILURE,
+    payload,
+  };
+};
+
+const logOut = () => {
+  return {
+    type: LOGOUT,
+  };
+};
+
+const loginUser = (payload) => (dispatch) => {
+  dispatch(loginRequest());
+  axios
+    .post("https://reddit-new.herokuapp.com/login", payload)
+    .then((res) => {
+      console.log("res:", res);
+
+      dispatch(loginSuccess(res.data));
+    })
+    .catch((err) => {
+      console.log("err:", err.response.data);
+
+      dispatch(loginFailure(err.response.data));
+    });
+};
+
+//..........................Register.................................//
+
+const registerRequest = () => {
+  return {
+    type: REGISTER_REQUEST,
+  };
+};
+
+const registerSuccess = (payload) => {
+  return {
+    type: REGISTER_SUCCESS,
+    payload,
+  };
+};
+
+const registerFailure = (payload) => {
+  return {
+    type: REGISTER_FAILURE,
+    payload,
+  };
+};
+
+const registerUser = (payload) => (dispatch) => {
+  dispatch(registerRequest());
+  axios
+    .post("https://reddit-new.herokuapp.com/register", payload)
+    .then((res) => {
+      dispatch(registerSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(registerFailure(err.response.data));
+    });
+};
+
+export { loginUser, registerUser, updateProfile, logOut };
---
+++ src/Redux/reducer.js
@@ -0,0 +1,107 @@
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT,
+  REGISTER_FAILURE,
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  UPDATE_PROFILE,
+} from "./actionType";
+
+const token = false;
+const user = false;
+
+const initState = {
+  isAuth: token ? true : false,
+  user: user || "",
+  token: token || "",
+  isLoading: false,
+  isError: false,
+  errMsg: "",
+  isLight: true,
+};
+
+export const authReducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    case LOGIN_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    }
+    case LOGIN_SUCCESS: {
+      //   saveData("token", payload.token);
+      //   saveData("user", payload.user);
+      return {
+        ...state,
+        isAuth: true,
+        user: payload.user,
+        token: payload.token,
+        isLoading: false,
+        isError: false,
+        errMsg: "",
+      };
+    }
+    case LOGIN_FAILURE: {
+      return {
+        ...state,
+        isAuth: false,
+        token: "",
+        isLoading: false,
+        isError: true,
+        errMsg: payload.message,
+      };
+    }
+    case REGISTER_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    }
+    case REGISTER_SUCCESS: {
+      //   saveData("token", payload.token);
+      //   saveData("user", payload.user);
+      return {
+        ...state,
+        isAuth: true,
+        user: payload.user,
+        token: payload.token,
+        isLoading: false,
+        isError: false,
+        errMsg: "",
+      };
+    }
+    case REGISTER_FAILURE: {
+      return {
+        ...state,
+        isAuth: false,
+        token: "",
+        isLoading: false,
+        isError: true,
+        errMsg: payload.message,
+      };
+    }
+    case UPDATE_PROFILE: {
+      //   saveData("user", payload);
+      return {
+        ...state,
+        user: payload,
+      };
+    }
+    case LOGOUT: {
+      //   saveData("token", "");
+      //   saveData("user", "");
+      return {
+        ...state,
+        isAuth: false,
+        user: "",
+        token: "",
+      };
+    }
+    default:
+      return state;
+  }
+};
